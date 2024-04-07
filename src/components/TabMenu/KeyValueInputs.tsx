25e9cf29d98f45fec5fa4574daef6adac3d0fa23
FahadulShadhin <fahadulshadhin17@gmail.com>
2024-04-07 23:37:58 +0600

    change button placement

--- src/components/TabMenu/KeyValueInputs.tsx
+++ src/components/TabMenu/KeyValueInputs.tsx
@@ -11,41 +11,45 @@ const KeyValueInputs = ({
   return (
     <div className="flex flex-col">
       {rows.map(({ key, value, description }, index) => (
-        <div
-          className={`flex border ${index !== 0 && 'border-t-0'} border-slate-600`}
-          key={index}
-        >
-          <input
-            type="text"
-            value={key}
-            onChange={(e) => handleChange(index, 'key', e.target.value)}
-            className="px-4 py-1 bg-inherit text-white placeholder-slate-500 focus:outline-none flex-grow focus:ring-0 border-r border-slate-600"
-            placeholder="Key"
-          />
-          <input
-            type="text"
-            value={value}
-            onChange={(e) => handleChange(index, 'value', e.target.value)}
-            className="px-4 py-1 bg-inherit text-white placeholder-slate-500 focus:outline-none flex-grow focus:ring-0 border-r border-slate-600"
-            placeholder="Value"
-          />
-          <input
-            type="text"
-            value={description}
-            onChange={(e) => handleChange(index, 'description', e.target.value)}
-            className="px-4 py-1 bg-inherit text-white placeholder-slate-500 focus:outline-none flex-grow focus:ring-0"
-            placeholder="Description"
-          />
-          {index === rows.length - 1 && (
-            <button className="pr-2" onClick={() => handleAddRow()}>
-              <Plus fill="#ffffff" height="12px" width="12px" />
-            </button>
-          )}
-          {rows.length !== 1 && (
-            <button className="pr-2" onClick={() => handleDeleteRow(index)}>
-              <TrashCan fill="#ffffff" height="12px" width="12px" />
-            </button>
-          )}
+        <div className='flex justify-between'>
+          <div
+            className={`flex border ${index !== 0 && 'border-t-0'} border-slate-600 ${rows.length === 1 ? 'w-[97%]': 'w-[95%]'}`}
+            key={index}
+          >
+            <input
+              type="text"
+              value={key}
+              onChange={(e) => handleChange(index, 'key', e.target.value)}
+              className="px-4 py-1 bg-inherit text-white placeholder-slate-500 focus:outline-none flex-grow focus:ring-0 border-r border-slate-600"
+              placeholder="Key"
+            />
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => handleChange(index, 'value', e.target.value)}
+              className="px-4 py-1 bg-inherit text-white placeholder-slate-500 focus:outline-none flex-grow focus:ring-0 border-r border-slate-600"
+              placeholder="Value"
+            />
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => handleChange(index, 'description', e.target.value)}
+              className="px-4 py-1 bg-inherit text-white placeholder-slate-500 focus:outline-none flex-grow focus:ring-0"
+              placeholder="Description"
+            />
+          </div>
+          <div className=''>
+            {index === rows.length - 1 && (
+              <button className="pr-2" onClick={() => handleAddRow()}>
+                <Plus fill="#ffffff" height="12px" width="12px" />
+              </button>
+            )}
+            {rows.length !== 1 && (
+              <button className="pr-2" onClick={() => handleDeleteRow(index)}>
+                <TrashCan fill="#ffffff" height="12px" width="12px" />
+              </button>
+            )}
+          </div>
         </div>
       ))}
     </div>
